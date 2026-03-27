fd525709e8d4fa55b2d3be38189ea9e21a40a4a2
Sergei Babrakov <s.a.babrakov@gmail.com>
2026-03-27 08:23:51 +0100

    Extends Thread

--- src/main/java/win/babrakov/Main.java
+++ src/main/java/win/babrakov/Main.java
@@ -4,21 +4,17 @@ package win.babrakov;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                throw new RuntimeException("Something bad happened");
-            }
-        };
+    public static void main(String[] args) {
+        Thread thread = new MyThread();
 
-        thread.setName("Misbehaving Thread");
-        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            @Override
-            public void uncaughtException(Thread t, Throwable e) {
-                System.out.println(t.getName() + " threw exception: " + e.getMessage());
-            }
-        });
         thread.start();
     }
+
+    private static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello from thread: " + Thread.currentThread().getName());
+        }
+    }
+
 }
