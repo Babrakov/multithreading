070fb7fa5be05f5b18e739f964b96621420326a3
Sergei Babrakov <s.a.babrakov@gmail.com>
2026-03-31 09:23:46 +0200

    Joining Threads

---
+++ src/main/java/win/babrakov/threadtermination/ComplexCalculation.java
@@ -0,0 +1,56 @@
+package win.babrakov.threadtermination;
+
+import java.math.BigInteger;
+
+public class ComplexCalculation {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(calculateResult(BigInteger.valueOf(10),
+                BigInteger.valueOf(2),
+                BigInteger.valueOf(2),
+                BigInteger.valueOf(3)));
+    }
+
+    public static BigInteger calculateResult(BigInteger base1, BigInteger power1, BigInteger base2, BigInteger power2)
+            throws InterruptedException {
+        BigInteger result;
+
+        PowerCalculatingThread thread1 = new PowerCalculatingThread(base1, power1);
+        PowerCalculatingThread thread2 = new PowerCalculatingThread(base2, power2);
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        result = thread1.getResult().add(thread2.getResult());
+
+
+        return result;
+    }
+
+    private static class PowerCalculatingThread extends Thread {
+        private BigInteger result = BigInteger.ONE;
+        private BigInteger base;
+        private BigInteger power;
+
+        public PowerCalculatingThread(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            BigInteger result = this.base;
+            BigInteger n = this.power;
+            while (n.compareTo(BigInteger.ONE)>0) {
+                result = result.multiply(this.base);
+                n = n.subtract(BigInteger.ONE);
+            }
+            this.result = result;
+        }
+
+        public BigInteger getResult() { return result; }
+    }
+}
